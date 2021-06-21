ee65ba14539d78f96d0f8a5ead1674df7cb7260a
AlejandroAndirkó <37534803+AGMAndirko@users.noreply.github.com>
2021-06-21 20:25:52 +0000

    Fleshed out comments
    - Changed some variable names
    - Added comments
    - Overall made nicer

--- rscript.R
+++ rscript.R
@@ -2,15 +2,17 @@ library(tidyverse)
 library(reshape2)
 library(gprofiler2)
 
+#Reads ExPecto output, including all genes under positive selection according to Peyregne et al. 2017
 out <- read_csv("output.csv")
 
+#Does GO enrichment analysis of genes on those genes
 go <- gprofiler2::gost(out$gene, evcodes = TRUE)
 glutamate_genes <- go[["result"]][14,]
 glutamate_genes <- glutamate_genes$intersection
 glutamate_genes <- strsplit(glutamate_genes, split = ",")
 
-#Divide output in glutamate signaling variants (and those that arent)
-glut_res <- function(exp_output, glut){
+# Divides ExPecto output into glutamate signaling variants (argument 2 = TRUE) and non-glutamatergic ones (arg2 = FALSE)
+glut_res <- function(exp_output, glut){ 
   exp_output <- exp_output[9:228]
   if ( glut == TRUE){
     out <- exp_output %>% 
@@ -23,6 +25,7 @@ glut_res <- function(exp_output, glut){
   return(mout)
 }
 
+# Averages values by tissue, regardless of gene
 avg_tissue <- function(df){
   out <- df %>% 
     group_by(variable) %>% 
@@ -30,6 +33,7 @@ avg_tissue <- function(df){
   return(out)
 }
 
+# Averages values by gene and tissue
 avg_gene <- function(df){
   out <- df %>% 
     group_by(gene, variable) %>% 
@@ -38,20 +42,24 @@ avg_gene <- function(df){
 }
 
 #Test for glut vs the rest, per tissue
+# Creates two dfs, one for glutamatergic genes and one that isn't
 glut <- glut_res(out, TRUE)
 non_glut <- glut_res(out, FALSE)
+
+# Average by tissue
 gluttiss <-avg_tissue(glut)
 NONgluttiss <-avg_tissue(non_glut)
 
-#Is taking into account 0s so probably skewed
+#Test2 is a dataframe to test average value variance between the two
 test2 <- cbind(gluttiss, NONgluttiss)
 colnames(test2) <- c("gluttis", "avgglut", "nongluttis", "avgnonglut")
 t.test(test2$avgnonglut, test2$avgglut, 
        data=test2,
        paired=TRUE,
        conf.level=0.95)
+# Significant
 
-#Generates dataframe for plot
+# Generates ex profeso dataframe for boxplot
 boxplot <-  NULL
 boxplot$values <- c(test2$avgglut, test2$avgnonglut) 
 boxplot$ids <- NA
@@ -59,8 +67,8 @@ boxplot$ids <- rep("Gutamatergic signalling", 218)
 boxplot$ids[219:436] <- rep("Control", 218)
 boxplot <- as.data.frame(boxplot)
 
-#Plot averages
-ggas("fig1.png")
+# Violin plot with averages per tissue (red dots) in glutamatergic and non glutamatergic signalling genes
+pdf("fig1.pdf")
 ggplot(boxplot, aes(ids, values))+ 
   theme_minimal() +
   geom_violin() +
@@ -70,17 +78,19 @@ ggplot(boxplot, aes(ids, values))+
   theme(legend.position = "none")
 dev.off()
 
-# Including everything
+# Generates ex profeso dataframe for an extra boxplot with every variant
 glut <- glut_res(out, TRUE)
 non_glut <- glut_res(out, FALSE)
 boxplot_everything <- NULL
 boxplot_everything$values <- c(glut$value, non_glut$value)
 boxplot_everything$ids <- NA
+# column "ids" denotes the group to which the variant belongs (glutamate, not glutamate)
 boxplot_everything$ids[1:length(glut$value)] <- rep("Glutamate", length(glut$value))
 boxplot_everything$ids[length(glut$value)+1:length(non_glut$value)] <- rep("Non glutamate", length(non_glut$value))
 boxplot_everything <- as.data.frame(boxplot_everything)
 
-#Plot 
+# Figure 2: boxplot with all variants
+# Note: Might be heavy or slow due to the number of variants
 pdf("boxplot.pdf")
 aaa <- ggplot(boxplot_everything, aes(ids, values))+ 
   theme_minimal() +
@@ -90,21 +100,25 @@ aaa <- ggplot(boxplot_everything, aes(ids, values))+
   theme(legend.position = "none")
 dev.off()
 
-
-#Test per gene
+# Testing significance per gene
+# Average contrast groups by tissue and gene
 glutgene <-avg_gene(glut)
 NONglutgene<-avg_gene(non_glut)
 
+# Test = ex profeso dataframe
 test <- NULL
 test <- rbind(glutgene, NONglutgene)
 test$id <- NA
 test$id[1:length(glutgene$avg_value)] <- "glut"
 test$id[length(glutgene$avg_value)+1:length(NONglutgene$avg_value)] <- "NONglut"
 
+# Linear model
 model <- lm(avg_value ~ id,
            data = test)
 summary(model)
+# Significant
 
+# Plot those values that are lower than -0.01 in both groups
 test <- test %>% 
   filter(avg_value < -0.01)
   
@@ -119,7 +133,7 @@ a <- ggplot(test, aes(avg_value, gene))+
 a <- a + facet_wrap(~ id)
 a
 
-# w
+# Creates another ex profeso dataframe, resets "test" as null to avoid confusion
 test <- NULL
 test <- rbind(glut, non_glut)
 test$id <-NA
@@ -129,14 +143,14 @@ test$id[length(glut$value)+1:length(non_glut$value)] <- rep("nonglut", length(no
 # Generates dataframe for cone plot
 genecone <- test %>% 
   group_by(gene, id, variable) %>% 
-  mutate(sum = sum(value)) %>% 
-  mutate(abs = sum(abs(value))) %>% 
+  mutate(sum = sum(value)) %>% # directional value per gene, tissue
+  mutate(abs = sum(abs(value))) %>%  # absolut value per gene, tissue
   select(gene, variable, id, sum, abs) %>% 
   unique()
-  #mutate(highlight, ifelse(abs > 10 && , TRUE, FALSE))
+  #mutate(highlight, ifelse(abs > 10 && , TRUE, FALSE)) # Creates a cuttof point to highlight
 
-
-# Extract brain tissues
+# Labels & highlights
+# Extracts brain tissues of interest to highlight in plot
 braintissues <- colnames(out[11:228])
 indx <- c(3,8,9, 10, 11, 12, 
        13, 14, 15, 16,17,
@@ -145,65 +159,57 @@ indx <- c(3,8,9, 10, 11, 12,
        189, 209)
 braintissues <- braintissues[indx]
 
+# Arrange dataframe by value, extract names from gene ids of top and bottom values for plot:
+lab <- genecone %>%
+  arrange(sum)
+lab$gene <- stringr::str_replace_all(lab$gene, "ENSG00000163618", "CADPS")
+lab$gene <- stringr::str_replace_all(lab$gene, "ENSG00000178235", "SLITRK1")
 
-
-pdf("test5.pdf")
+# Coneplot, highlighting brain tissues though ifelse conditionals + labels for extreme values
+pdf("coneplot.pdf")
 a1 <- ggplot(genecone, aes(sum, abs)) +
   theme_minimal() +
   coord_flip() +  
-  geom_point(colour = 'grey', size = 0.1 ) 
-
-a1 <- a1 + geom_point(colour = (ifelse((genecone$id == "glut" & !(genecone$variable %in% braintissues)), 
+  geom_point(colour = 'grey', size = 0.1 ) + 
+  geom_point(colour = (ifelse((genecone$id == "glut" & !(genecone$variable %in% braintissues)), 
                       '#440154FF', NA)), size = 0.3) +
   geom_point(colour = (ifelse((genecone$id == "glut" & (genecone$variable %in% braintissues)), 
                                  '#20A387FF', NA)), size = 0.3) +
-  labs(title="", x ="Sum of absolute expression", y = "Absolut sum of absolute expression")
-
-dev.off()
-
-nrs <- c(-0.2282983)
-
-h <- genecone %>%
-  arrange(sum)
-
-h$gene <- stringr::str_replace_all(h$gene, "ENSG00000163618", "CADPS")
-#test <- gprofiler2::gconvert(genecone$gene)
-h$gene <- stringr::str_replace_all(h$gene, "ENSG00000178235", "SLITRK1")
-
-
-
-a1 + 
+  labs(title="", x ="Sum of absolute expression", y = "Absolut sum of absolute expression") + 
   geom_label_repel(data = h[3,], aes(x=sum, y=abs, label=paste(gene, "-", variable)), box.padding = 0.5, max.overlaps = Inf) +
   geom_label_repel(data = h[90408,], aes(x=sum, y=abs, label=paste(gene, "-", variable)), box.padding = 0.5, max.overlaps = Inf) 
-  
-#313-314 776 1650 3086-3088 grik5
-#856-857 1210-1211 1810-1811 3370 -3378 slc12a5
-#1302 2621-2623 SORCS1
-# 2347-2350 ZC3H12A
-#4275-4277 SLC1A1
+dev.off()
+
+
+# join both glutamate and non-glutamate dataframes (should be the same as "out")
 all <- rbind(glut, non_glut)
-all2 <- all %>% 
+
+# Take out 0's because they mess up the z score
+all <- all %>% 
   group_by(gene) %>% 
   summarize(value = sum(value)) %>% 
   filter(value != 0)
 
-all2  <- all2 %>% 
+# Calculates z scores and local FDR
+all  <- all %>% 
   mutate(z_scores = value-mean(value)/sd(value))
+fdr.out <- fdrtool(all[[3]], statistic="normal")
 
-fdr.out <- fdrtool(all2[[3]], statistic="normal")
-
-h <- all2 %>%
+# Creates df for highlights
+highlight <- all %>%
   filter(glutamateornot == TRUE)
 
-DE <- all2 %>% 
+#Filters genes by false discovery rate cutoff
+DE <- all %>% 
   filter(fdr < 0.01)
 
+# Plot genes with higher than expected expression, plus label for the glutamate genes
 pdf("DEplot.pdf")
-ggplot(data=all2, aes(x=value, y=-1*log10(fdr))) +
+ggplot(data=all, aes(x=value, y=-1*log10(fdr))) +
   theme_minimal() +
   geom_point() +
   geom_point(data = DE, aes(x=z_scores, y=-1*log10(fdr)), color = "red") +
-  geom_label_repel(data = h, aes(x=z_scores, y=-1*log10(fdr), label=name), box.padding = 1, max.overlaps = Inf) +
+  geom_label_repel(data = highlight, aes(x=z_scores, y=-1*log10(fdr), label=name), box.padding = 1, max.overlaps = Inf) +
   labs(title="Differentially expressed genes", x ="Aggregated expression value", y ="1-log10(FDR)")
 dev.off()
 
